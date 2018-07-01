5b546e3caed4bbafda008f6bff52d5fd15696a0a
Taufik Arifuddin <taufikarifuddin@gmail.com>
2018-07-01 14:54:11 +0700

    fix node emitter name + delete order when reject

--- _protected/controllers/OperationController.php
+++ _protected/controllers/OperationController.php
@@ -45,43 +45,67 @@ class OperationController extends Controller{
 
     public function actionConfirm(){
         $post = Yii::$app->request->post();
+        $rejectedItem = [];        
+        $dataTemp = [];
         if( $post && Yii::$app->request->isAjax ){
             foreach( $post['data'] as $k => $v ){
                 $orderItem = OrderItem::findOne($v['id']);
                 if( is_null($orderItem) ){
                     return false;
                 }
-                $orderItem->approved = $v['approved'] ? true : false;
-                if( !$v['approved'] ){
-                    $orderItem->approved = $v['note'];
+                $orderItem->approved = $v['approved'] == 'true'? true : false;
+                if( !$orderItem->approved ){
+                    $orderItem->note = $v['note'];
+                    $rejectedItem[] = [
+                        'food' => $orderItem->food->name,
+                        'note' => $orderItem->note
+                    ];
                 }
-                if( !$orderItem->save() ){
+                if( !$orderItem->save() ){                    
                     var_dump($orderItem->errors);
                     return false;
                 }
+                $dataTemp[] = [
+                    'qty' => $orderItem->qty,
+                    'foodId' => $orderItem->food_id
+                ];
             }
 
             $order = Order::findOne($post['order']);
             if( is_null($order) ){
                 return false;
             }
-            $order->step = OrderStep::DOING_BY_KOKI;
-            if( !$order->save() ){
-                var_dump($order->errors);
-                return false;
+
+            if( count($rejectedItem) == 0 ){
+                $order->step = OrderStep::DOING_BY_KOKI;
+                if( !$order->save() ){
+                    var_dump($order->errors);
+                    return false;
+                }
+
+                NotificationService::emit(EmiterModel::SUBMIT_TO_CHEF,[
+                    'orderId' => $order->id
+                ]);  
+
+                NotificationService::emit(EmiterModel::NOTIFY_PROGRESS,[
+                    'orderId' => $order->id,
+                    'status' => \app\models\OrderStep::DOING_BY_KOKI,
+                    'date' => $order->order_time
+                ]);  
+                return true;
+            }else{
+
+                $orderObj = Order::findOne($post['order']);
+                if( !is_null($orderObj) ){
+                    $orderObj->delete();
+                }
+                
+                NotificationService::emit(EmiterModel::SEND_BACK_TO_USER,[
+                    'reject' => $rejectedItem,
+                    'orderItem' => $dataTemp
+                ]);  
+                return true;
             }
-
-            NotificationService::emit(EmiterModel::SUBMIT_TO_CHEF,[
-                'orderId' => $order->id
-            ]);  
-
-            NotificationService::emit(EmiterModel::NOTIFY_PROGRESS,[
-                'orderId' => $order->id,
-                'status' => \app\models\OrderStep::DOING_BY_KOKI,
-                'date' => $order->order_time
-            ]);  
-
-            return true;
         }
 
 
--- node-server/server.js
+++ node-server/server.js
@@ -21,7 +21,7 @@ io.on('connection', function(socket){
     })
     
     socket.on(SEND_BACK_TO_USER, function(data) { 
-      io.emit(SUBMIT_TO_CHEF,data);
+      io.emit(SEND_BACK_TO_USER,data);
     });
     
     socket.on(SUBMIT_TO_CHEF, function(data) { 
